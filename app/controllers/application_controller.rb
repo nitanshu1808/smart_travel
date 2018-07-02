96c30d198c9ba464504d552e7c8eb12ec1040813
Nitanshu Rehani <nitanshurehani@Nitanshus-MacBook-Air.local>
2018-07-02 17:56:44 +0100

    navbar collapse on mobile view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+	protected
+    def after_sign_in_path_for(resource)
+      stored_location_for(resource) || root_url
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,18 @@
 module ApplicationHelper
-	def resource_name
+  def resource_name
     :user
   end
 
   def resource
     @user ||= User.new
   end
+
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def check_redirect_url
+    current_user.present? && 'javascript:void(0)' || root_url   
+  end
+
 end
