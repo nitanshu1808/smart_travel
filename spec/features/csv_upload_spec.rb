a9961e32c81e9de86d3d25251c67ef256818a5fd
Nitanshu Rehani <nitanshurehani@nitanshus-air.home>
2018-09-04 23:27:14 +0100

    - Fragment cache implementation - rspec for csv upload

--- spec/features/csv_upload_spec.rb
+++ spec/features/csv_upload_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
+include ActionDispatch::TestProcess
 
 
 RSpec.feature "user registration" do
   it "using instagram" do
     visit pokemons_path
-    expect(page).to have_content("Pokemons")
-    expect(page).to have_button("Import data from CSV")
+    expect(page).to have_content(I18n.t("web.pokemons"))
+    expect(page).to have_button(I18n.t("web.import_csv"))
+    attach_file('file', "#{Rails.root}/spec/fixtures/files/pokemon.csv")
+    find('input[name="commit"]').click
+    expect(page).to have_content(I18n.t("web.record_created"))
   end
 end
